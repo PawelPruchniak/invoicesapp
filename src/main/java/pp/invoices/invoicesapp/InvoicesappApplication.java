ef65fa68c6c02740b8c1778b2f418ac188dea834
PawelPruchniak <pw.pruchniak@gmail.com>
2021-09-21 17:43:39 +0200

    Initial commit

---
+++ src/main/java/pp/invoices/invoicesapp/InvoicesappApplication.java
@@ -0,0 +1,13 @@
+package pp.invoices.invoicesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicesappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pp/invoices/invoicesapp/InvoicesappApplicationTests.java
@@ -0,0 +1,13 @@
+package pp.invoices.invoicesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
