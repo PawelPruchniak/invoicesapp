082a9119533b97d2cfd6cf030c1229b149d2fd6b
PawelPruchniak <pw.pruchniak@gmail.com>
2021-09-25 13:59:55 +0200

    Created invoice item static strings

---
+++ src/main/java/pp/invoices/invoicesapp/enums/InvoiceItemStrings.java
@@ -0,0 +1,18 @@
+package pp.invoices.invoicesapp.enums;
+
+public class InvoiceItemStrings {
+    public static final String INVOICE_ITEM_ATTRIBUTE = "invoiceItem";
+    public static final String INVOICE_ITEMS_ATTRIBUTE = "invoiceItems";
+
+    public static final String ID_ATTRIBUTE = "id";
+    public static final String CUSTOMER_ID_ATTRIBUTE = "customer";
+    public static final String AMOUNT_ATTRIBUTE = "amount";
+    public static final String CURRENCY_ATTRIBUTE = "currency";
+    public static final String DESCRIPTION_ATTRIBUTE = "description";
+
+    public static final String LIMIT_ATTRIBUTE = "limit";
+
+    public static final String INVOICE_ITEM_POST_RESULT = "invoice_item_post_result";
+    public static final String INVOICE_ITEM_FORM = "invoice_item_post_form";
+    public static final String INVOICE_ITEMS_LIST = "invoice_item_list";
+}
